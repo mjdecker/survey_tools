5b983c4b8cc2a840d96791fdb52ee963f40632da
Michael John Decker <mdecker6@kent.edu>
2016-03-08 09:36:05 -0500

    Sample 3 modified code.

--- tool1/sample3.java
+++ tool1/sample3.java
@@ -1,23 +1,18 @@
 @Test
 public void passQueryAsJSONStringTest() throws Exception {
-    client().admin().indices().prepareCreate("test").setSettings(
-        ImmutableSettings.settingsBuilder().put("index.number_of_shards", 1)).execute().actionGet();
+    assertAcked(prepareCreate("test").setSettings(SETTING_NUMBER_OF_SHARDS,
+         1));
 
     client().prepareIndex("test", "type1", "1")
         .setSource("field1", "value1_1", "field2", "value2_1")
-        .setRefresh(true).execute().actionGet();
+        .setRefresh(true).get();
 
     WrapperQueryBuilder wrapper
          = new WrapperQueryBuilder("{ \"term\" : { \"field1\" : \"value1_1\" } }");
-    CountResponse countResponse = client().prepareCount()
-        .setQuery(wrapper).execute().actionGet();
-    assertHitCount(countResponse, 1l);
+    assertHitCount(client().prepareCount()
+        .setQuery(wrapper).get(), 1l);
 
-    BoolQueryBuilder bool = new BoolQueryBuilder();
-    bool.must(wrapper);
-    bool.must(new TermQueryBuilder(
+    BoolQueryBuilder bool = boolQuery().must(wrapper).must(new TermQueryBuilder(
         "field2", "value2_1"));
-
-    countResponse = client().prepareCount().setQuery(wrapper).execute().actionGet();
-    assertHitCount(countResponse, 1l);
+    assertHitCount(client().prepareCount().setQuery(bool).get(), 1l);
 }
